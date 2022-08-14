b5e9b063eb06927b26185f2194288924ae6fe6b4
chassitynakamura <nakamurachassity@gmail.com>
2022-08-14 08:38:47 -0700

    Complete Mockserver testing

--- dist/src/components/App/App.test.js
+++ dist/src/components/App/App.test.js
@@ -15,13 +15,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
 // External Dependencies
 const enzyme_1 = require("enzyme");
+const axios_1 = __importDefault(require("axios"));
+const react_1 = require("@testing-library/react");
 const Display_styled_1 = require("../Display/Display.styled");
 // Internal Dependencies
 const App_1 = __importDefault(require("./App"));
 const testInput = (btnGroup, expr) => __awaiter(void 0, void 0, void 0, function* () {
     for (let ch of expr) {
         if (ch >= "0" && ch <= "9") {
-            btnGroup.numBtn.simulate("click");
+            btnGroup.numBtn[Number(ch)].simulate("click");
         }
         if (ch === "+")
             yield btnGroup.plusBtn.simulate("click");
@@ -42,26 +44,77 @@ const testInput = (btnGroup, expr) => __awaiter(void 0, void 0, void 0, function
 const getButtonGroup = () => {
     const app = (0, enzyme_1.mount)((0, jsx_runtime_1.jsx)(App_1.default, {}));
     const display = app.find(Display_styled_1.StyleScreen);
-    // const plusBtn = app.find({children: '+'})
+    const plusBtn = app.find("[id='+']");
     // const minusBtn =  app.find({children:})
     // const timesBtn = app.find({children:})
     // const divBtn = app.find({children:})
     // const pointBtn = 
     // const button = shallow((<Button onClick={mockCallBack}>6</Button>));
     // const numBtn = button.find("button")
-    const numBtn = app.find("[id='6']");
+    const numBtn = Array(10);
+    for (let i = 0; i < 10; i++)
+        numBtn[i] = app.find(`[id=${i}]`);
     return {
         app,
         display,
+        plusBtn,
         numBtn
     };
+};
+jest.mock("axios");
+const getMockedAxios = () => {
+    const mockedAxios = axios_1.default;
+    mockedAxios.get.mockImplementation((url) => {
+        const searchParams = new URLSearchParams(url.slice(url.indexOf("?") + 1));
+        if (searchParams == null)
+            return Promise.reject();
+        let operation = searchParams.get("operation");
+        let total = searchParams.get("total");
+        let next = searchParams.get("next");
+        let rlt;
+        let totalNum = isNaN(Number(total)) ? null : Number(total);
+        let nextNum = isNaN(Number(next)) ? null : Number(next);
+        if (nextNum == null) {
+            rlt = totalNum ? totalNum : "0";
+        }
+        else {
+            switch (operation) {
+                case "plus":
+                    rlt = totalNum + nextNum;
+                    break;
+                case "minus":
+                    rlt = totalNum - nextNum;
+                    break;
+                case "times":
+                    rlt = totalNum * nextNum;
+                    break;
+                case "divide":
+                    rlt = totalNum / nextNum;
+                    break;
+                default: {
+                    rlt = nextNum;
+                }
+            }
+        }
+        rlt = String(rlt);
+        return Promise.resolve({
+            data: rlt,
+        });
+    });
+    return mockedAxios;
 };
 describe('test calculation', () => {
     it('input validation', () => {
         const btngroup = getButtonGroup();
         // console.log(btngroup.display.children())
         testInput(btngroup, "6");
-        expect(btngroup.numBtn.getDOMNode().innerHTML).toEqual("6");
-        // expect(btngroup.display.getDOMNode().innerHTML).toEqual("6");
+        expect(btngroup.display.getDOMNode().innerHTML).toEqual("6");
     });
+    it("test plus", () => __awaiter(void 0, void 0, void 0, function* () {
+        const btnGroup = getButtonGroup();
+        const mockedAxios = getMockedAxios();
+        testInput(btnGroup, "123+45+6=");
+        yield (0, react_1.waitFor)(() => expect(mockedAxios.get).toHaveBeenCalledTimes(5));
+        expect(btnGroup.display.getDOMNode().innerHTML).toEqual("174");
+    }));
 });
--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -24,7 +24,7 @@ import App from './App';
 const testInput = async (btnGroup: any, expr: string) => {
     for (let ch of expr) {
         if (ch >= "0" && ch <= "9") {
-            btnGroup.numBtn.simulate("click");
+            btnGroup.numBtn[Number(ch)].simulate("click");
         }
         if (ch === "+") await btnGroup.plusBtn.simulate("click");
         if (ch === "-") await btnGroup.minusBtn.simulate("click");
@@ -41,7 +41,8 @@ const testInput = async (btnGroup: any, expr: string) => {
 const getButtonGroup = () => {  
     const app = mount(<App/>);
     const display = app.find(StyleScreen);
-    // const plusBtn = app.find({children: '+'})
+    const plusBtn = app.find("[id='+']");
+    const eqBtn = app.find("[id='=']");
     // const minusBtn =  app.find({children:})
     // const timesBtn = app.find({children:})
     // const divBtn = app.find({children:})
@@ -49,14 +50,67 @@ const getButtonGroup = () => {
 
     // const button = shallow((<Button onClick={mockCallBack}>6</Button>));
     // const numBtn = button.find("button")
-    const numBtn = app.find("[id='6']")
+    const numBtn = Array(10);
+    for (let i = 0; i < 10; i++) numBtn[i] = app.find(`[id="${i}"]`);
     return {
         app,
         display,
+        plusBtn,
+        eqBtn,
         numBtn
     }
 
 }
+
+
+jest.mock("axios");
+
+const getMockedAxios = () => {
+    const mockedAxios = axios as jest.Mocked<typeof axios>;
+    mockedAxios.get.mockImplementation((url) => {
+        const searchParams = new URLSearchParams(
+            url.slice(url.indexOf("?") + 1)
+        );
+        if (searchParams == null) return Promise.reject();
+
+        let operation = searchParams.get("operation");
+        let total = searchParams.get("total");
+        let next = searchParams.get("next");
+        let rlt;
+
+        let totalNum = isNaN(Number(total)) ? null : Number(total);
+        let nextNum = isNaN(Number(next)) ? null : Number(next);
+        
+        if (nextNum == null) {
+            rlt = totalNum ? totalNum : "0";
+        } else {
+            switch (operation) {
+                case "plus":
+                    rlt = totalNum as number + nextNum;
+                    break;
+                case "minus":
+                    rlt = totalNum as number - nextNum;
+                    break;
+                case "times":
+                    rlt = totalNum as number * nextNum;
+                    break;
+                case "divide":
+                    rlt = totalNum as number / nextNum;
+                    break;
+                default: {
+                    rlt = nextNum;
+                }
+            }
+        }
+        rlt = String(rlt);
+
+        return Promise.resolve({
+            data: rlt,
+        });
+    });
+    return mockedAxios;
+};
+
 
 describe('test calculation', () => {
     it('input validation', () => {
@@ -64,7 +118,17 @@ describe('test calculation', () => {
         const btngroup =  getButtonGroup();
         // console.log(btngroup.display.children())
         testInput(btngroup, "6");
-        expect(btngroup.numBtn.getDOMNode().innerHTML).toEqual("6")
-        // expect(btngroup.display.getDOMNode().innerHTML).toEqual("6");
-    })
+
+        expect(btngroup.display.getDOMNode().innerHTML).toEqual("6");
+    });
+    
+    it("test plus", async () => {
+        const btnGroup = getButtonGroup();
+        const mockedAxios = getMockedAxios();
+
+
+        testInput(btnGroup, "123+45+6=");
+        await waitFor(() => expect(mockedAxios.get).toHaveBeenCalledTimes(3));
+        expect(btnGroup.display.getDOMNode().innerHTML).toEqual("174");
+    });
 })
