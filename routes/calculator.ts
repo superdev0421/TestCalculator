1d55b4161ce57ec888094faff44cc26b9bccc482
superdev0421 <101011021+superdev0421@users.noreply.github.com>
2024-03-27 19:46:32 -0700

    Revert "Revert "clean up; remove logs""

--- routes/calculator.ts
+++ routes/calculator.ts
@@ -4,39 +4,35 @@ import { Request, Response } from "express";
 const router = Router();
 
 const calcMain = (req: Request, res: Response) => {
+  const { total, next, operation } = req.query as any;
 
-    const { total, next, operation } = req.query as any;
-    // console.log('total', total);
-    // console.log('next', next);
-    // console.log('operation', operation);
+  let totalNum: number | null = isNaN(total) ? null : Number(total);
+  let nextNum: number | null = isNaN(next) ? null : Number(next);
 
-    let totalNum: number | null = isNaN(total) ? null : Number(total);
-    let nextNum: number | null = isNaN(next) ? null : Number(next);
-    
-    if (nextNum == null) {
-        res.send(totalNum ? totalNum.toString() : "0");
-        return;
+  if (nextNum == null) {
+    res.send(totalNum ? totalNum.toString() : "0");
+    return;
+  }
+
+  switch (operation) {
+    case "plus":
+      res.send(((totalNum as number) + nextNum).toString());
+      break;
+    case "minus":
+      res.send(((totalNum as number) - nextNum).toString());
+      break;
+    case "times":
+      res.send(((totalNum as number) * nextNum).toString());
+      break;
+    case "divide":
+      res.send(((totalNum as number) / nextNum).toString());
+      break;
+    default: {
+      res.send(nextNum.toString());
     }
-
-    switch (operation) {
-        case "plus":
-            res.send((totalNum as number + nextNum).toString());
-            break
-        case "minus":
-            res.send((totalNum as number - nextNum).toString());
-            break
-        case "times":
-            res.send((totalNum as number * nextNum).toString());
-            break
-        case "divide":
-            res.send((totalNum as number / nextNum).toString());
-            break
-        default: {
-            res.send(nextNum.toString());
-        }
-    }
+  }
 };
 
-router.get('/', calcMain);
+router.get("/", calcMain);
 
 export default router;
